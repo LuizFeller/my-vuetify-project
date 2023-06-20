19896432f57db0de8daf638693b9da0c798c4556
LuizFeller <luiz.feller@terra.com.br>
2023-06-20 09:48:58 -0300

    vp-19062023

--- src/views/Form3.vue
+++ src/views/Form3.vue
@@ -5,8 +5,7 @@ export default {
     banana: [
       (value) => {
         if (!value) return "Please say something";
-        if (!value.includes(""))
-        return "You must enter a first name.";
+        if (!value.includes("")) return "You must enter a first name.";
         return true;
       },
     ],
@@ -30,15 +29,19 @@ export default {
   <v-sheet width="300" class="mx-auto">
     <v-form @submit.prevent v-model="isFormValid">
       <v-text-field
-      v-model="firstName"
-      :rules="banana"
-      label="Full name"
+        v-model="firstName"
+        :rules="banana"
+        label="Full name"
       ></v-text-field>
       <v-btn
-      type="submit"
-      :disabled="isFormValid"
-      @click="handSubmit"
-      block class="mt-2">Submit</v-btn>
+        type="submit"
+        :disabled="isFormValid"
+        @click="handSubmit"
+        block
+        class="mt-2"
+        >Submit</v-btn
+      >
     </v-form>
   </v-sheet>
-</template> -->
+</template>
+ -->
