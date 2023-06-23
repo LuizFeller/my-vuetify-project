314b51392abb9a5093ea7a0293085d6afc82a92d
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-23 12:06:01 -0300

    cria chamada para detalhe da lista

--- src/api/toDoLists.js
+++ src/api/toDoLists.js
@@ -5,5 +5,8 @@ export const toDoListApiMixin = {
         async list() {
             return await privateApi.get("/lists");
         },
+        async detailList(id) {
+            return await privateApi.get(`/lists/${id}`);
+        },
     },
 };
