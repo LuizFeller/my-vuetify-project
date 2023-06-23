c9babae7fc2c7a5ebdfeebd086d468c532b68848
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-23 13:57:18 -0300

    Usa método de criar e apagar listas

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,6 +14,7 @@ export default {
     return {
       lists: '',
       items: '',
+      titleDialog: '',
       openDialog: false,
       openNewList: false,
     }
@@ -36,7 +37,38 @@ export default {
       } catch {
         alert("Deu erro... D:")
       }
-    }
+    },
+    async createNewList(title) {
+      const { status } = await this.createList(title)
+      this.openNewList = false
+
+      console.log(status);
+
+      if (status >= 200 && status < 300) {
+        this.getLists()
+        alert("Deu boa!")
+      } else {
+        alert("Deu erro")
+      }
+
+    },
+    async handleDeleteItem(id) {
+      const { status } = await this.deleteList(id);
+
+      if (status >= 200 && status < 300) {
+        this.getLists()
+        alert("Deu boa!")
+      } else {
+        alert("Deu erro")
+      }
+    },
+
+
+    /* METODO DE ESTILIZAÇÂO */
+    btnColor(a) {
+      console.log(a);
+      return a == true ? 'success' : 'error'
+    },
   },
   mounted() {
     this.getLists();
@@ -47,7 +79,7 @@ export default {
 <template>
   <div>
     <nav class="w-100 bg-blue d-flex justify-center">
-      <v-btn @click="openNewList = true" variant="outlined ma-2">
+      <v-btn @click="openNewList = true" variant="plain">
         CRIAR LISTA
       </v-btn>
     </nav>
@@ -59,10 +91,10 @@ export default {
         <v-btn color="blue" @click="handleDetailItens(list.id)">
           DETALHE
         </v-btn>
-        <v-btn color="blue" @click="handleDetailItens(list.id)">
+        <v-btn color="blue">
           EDITAR
         </v-btn>
-        <v-btn color="blue" @click="handleDetailItens(list.id)">
+        <v-btn color="blue" @click="handleDeleteItem(list.id)">
           DELETAR
         </v-btn>
       </v-card-actions>
@@ -70,25 +102,28 @@ export default {
 
 
     <!-- DIALOG - ITENS IN LIST -->
-    <v-dialog v-model="openDialog" width="auto" v-show="openDialog">
-      <v-card>
 
-        <v-card v-for="item in items" class="d-flex align-center">
+    <v-dialog v-model="openDialog" width="auto" v-show="openDialog">
+      <v-card class="rounded-lg">
+        <v-card v-for="item in items" class="d-flex align-center ma-2 pa-2 elevation-4">
           <v-card-title> {{ item.title }} </v-card-title>
-          <v-card-text> {{ item.deadline }} </v-card-text>
-          <v-card-text> {{ item.deadline }} </v-card-text>
-          <v-card-text> {{ item.done }} </v-card-text>
+          <v-card-text>{{ item.deadline }}</v-card-text>
+          <v-card-text>{{ item.createdAt }}</v-card-text>
+          <v-card-text>{{ item.updatedAt }}</v-card-text>
+          <v-btn :color="btnColor(item.done)">Concluir</v-btn>
         </v-card>
 
         <v-card-actions>
-          <v-btn color="primary" block @click="openDialog = false">Close Dialog</v-btn>
+          <v-btn color="primary" block @click="openDialog = false">Ok</v-btn>
         </v-card-actions>
 
       </v-card>
     </v-dialog>
 
     <!-- CRIAR NOVA LISTA -->
-    <ModalNewList v-if="openNewList"></ModalNewList>
+    <ModalNewList @new-list="createNewList" v-if="openNewList"></ModalNewList>
 
   </div>
 </template>
+
+<style></style>
