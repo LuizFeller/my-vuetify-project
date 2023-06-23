d8d4969511d932b970a7b2b3d7d006e2dde0ee99
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-23 13:57:04 -0300

    usa método para criar lista no componente

--- src/components/modal-new-list.vue
+++ src/components/modal-new-list.vue
@@ -1,10 +1,26 @@
-<template>
-    <v-card class="modal d-flex align-center justify-center">
-        <v-card class="h-25 w-25 d-flex align-center flex-column">
-            <v-card-title>Nome da Lista</v-card-title>
-            <v-text-field>aaaa</v-text-field>
-            <v-btn>Criar</v-btn>
+<script>
 
+export default {
+    data(){
+        return{
+            listName: '',
+        }
+    },
+    methods: {
+        handleCreateList(){
+            this.$emit('new-list', this.listName)
+        }
+    }
+}
+
+</script>
+
+<template>
+    <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
+        <v-card class="h-25 w-25 pa-4">
+            <v-card-title>Nome da Lista</v-card-title>
+            <v-text-field v-model="listName"></v-text-field>
+            <v-btn @click="handleCreateList">Criar</v-btn>
         </v-card>
 
     </v-card>
@@ -12,7 +28,6 @@
 
 <style scoped>
 .modal {
-    background-color: rgba(0, 0, 0, 0.5);
     position: fixed;
     top: 0;
     right: 0;
