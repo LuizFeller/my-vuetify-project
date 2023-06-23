8ed4268e0fd27bff26693e811d864a8123033106
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-23 12:06:13 -0300

    cria modal de criação de nova lista

---
+++ src/components/modal-new-list.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card class="modal d-flex align-center justify-center">
+        <v-card class="h-25 w-25 d-flex align-center flex-column">
+            <v-card-title>Nome da Lista</v-card-title>
+            <v-text-field>aaaa</v-text-field>
+            <v-btn>Criar</v-btn>
+
+        </v-card>
+
+    </v-card>
+</template>
+
+<style scoped>
+.modal {
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
