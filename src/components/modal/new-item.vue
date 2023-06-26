6832e959d9fc8b96d058a07d373b9f927821a1df
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-26 14:41:08 -0300

    Formata o deadline, classifica lista e o apresenta

--- src/components/modal/new-item.vue
+++ src/components/modal/new-item.vue
@@ -4,11 +4,14 @@ export default {
     data(){
         return{
             itemName: '',
+            deadLineDate: '',
+            deadLineHour: '',           
         }
     },
     methods: {
-        handleCreateList(){
-            this.$emit('new-item', this.itemName)
+        handleCreateItemList(){
+            const formatedDate = `${this.deadLineDate}T${this.deadLineHour}:00.000Z`
+            this.$emit('new-item', this.itemName, formatedDate)
         }
     }
 }
@@ -17,10 +20,12 @@ export default {
 
 <template>
     <v-card color="rgba(0, 0, 0, 0.5)" class="modal d-flex align-center justify-center">
-        <v-card class="h-25 w-25 pa-4">
+        <v-card class="w-25 pa-4">
             <v-card-title>Nome da Tarefa</v-card-title>
             <v-text-field v-model="itemName"></v-text-field>
-            <v-btn @click="handleCreateList">Criar</v-btn>
+            <v-text-field v-model="deadLineDate" type="date"></v-text-field>
+            <v-text-field v-model="deadLineHour" type="time"></v-text-field>
+            <v-btn @click="handleCreateItemList">Criar</v-btn>
         </v-card>
     </v-card>
 </template>
