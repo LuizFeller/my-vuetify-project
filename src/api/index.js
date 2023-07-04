09281e67e3c5f26751fe2e0c8c6e4e8293072cb1
LuizFeller <luiz.feller@terra.com.br>
2023-07-04 09:20:31 -0300

    teste

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,27 @@
 import axios from "axios";
 
 export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+
+  //http://to-do-list-api-dev.us-east-2.elasticbeanstalk.com/
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+export let privateApi;
+
+export const setupPrivateApi = (token) => {
+  privateApi = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 
     //http://to-do-list-api-dev.us-east-2.elasticbeanstalk.com/
 
     headers: {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
-});
-export let privateApi;
-
-export const setupPrivateApi = (token) => {
-    privateApi = axios.create({
-        baseURL:  import.meta.env.VITE_API_URL,
-        
-        //http://to-do-list-api-dev.us-east-2.elasticbeanstalk.com/
-        //http://localhost:3333
-
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-    });
+  });
 };
 
 /* api.interceptors.request.use((config) => {
